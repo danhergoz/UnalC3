5fb13ba7d9ee805ff5c924c33a7ebd0142b8656d
Jack <l.jmwr7x@gmail.com>
2021-09-16 21:23:34 -0500

    Interfaz de saludo.

--- Nuevo.py
+++ Nuevo.py
@@ -1,4 +1,8 @@
 import tkinter as tk
+from tkinter import messagebox
+
+def saludar():
+    messagebox.showinfo('Saludando', 'Hola Mundo')
 
 print("Hola mundo")
 print("Hola mundo")
@@ -6,3 +10,13 @@ print("Hola mundo")
 print("Hola mundo por deiber")
 print("Hola mundo (Daniel H)")
 print("Hola mundo Oscar")
+
+root = tk.Tk()
+
+frame = tk.Frame(root, bg = 'red')
+frame.pack()
+
+button = tk.Button(frame, text = 'Saludar', command = lambda:saludar())
+button.pack()
+
+root.mainloop()
