309b6676f3a8f3b4e84305b5cc805d99213b2046
Jack <l.jmwr7x@gmail.com>
2021-09-16 21:37:11 -0500

    Interfaz de saludo.

--- Nuevo.py
+++ Nuevo.py
@@ -3,7 +3,11 @@ from tkinter import messagebox
 from tkinter import colorchooser
 
 def saludar():
-    messagebox.showinfo('Saludando', 'Hola Mundo')
+    texto = caja_texto.get()
+    if texto == '':
+        messagebox.showinfo('Saludando', 'Hola Mundo')
+    else:
+        messagebox.showinfo('Saludando', 'Hola ' + texto)
 
 def colorear():
     color = colorchooser.askcolor()[1]
@@ -17,14 +21,21 @@ print("Hola mundo (Daniel H)")
 print("Hola mundo Oscar")
 
 root = tk.Tk()
+root.resizable(0,0)
 
 frame = tk.Frame(root, bg = 'red')
 frame.pack()
 
+label_nombre = tk.Label(frame, text = 'Nombre:', bg = 'red')
+label_nombre.grid(row = 0, column = 0, padx = 5, pady = 5)
+
+caja_texto = tk.Entry(frame)
+caja_texto.grid(row = 0, column = 1, padx = 5, pady = 5)
+
 button1 = tk.Button(frame, text = 'Saludar', command = lambda:saludar())
-button1.grid(row = 0, column = 0, padx = 5, pady = 5)
+button1.grid(row = 1, column = 0, padx = 5, pady = 5)
 
 button2 = tk.Button(frame, text = 'Escoje un color', command = lambda:colorear())
-button2.grid(row = 0, column = 1, padx = 5, pady = 5)
+button2.grid(row = 1, column = 1, padx = 5, pady = 5)
 
 root.mainloop()
