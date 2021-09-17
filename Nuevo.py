1a30f3478b2e762d100394dc6ddd84847aa6121a
Jack <l.jmwr7x@gmail.com>
2021-09-16 21:29:47 -0500

    Interfaz de saludo 2.

--- Nuevo.py
+++ Nuevo.py
@@ -1,8 +1,13 @@
 import tkinter as tk
 from tkinter import messagebox
+from tkinter import colorchooser
 
 def saludar():
     messagebox.showinfo('Saludando', 'Hola Mundo')
+
+def colorear():
+    color = colorchooser.askcolor()[1]
+    frame.config(bg = color)
 
 print("Hola mundo")
 print("Hola mundo")
@@ -16,7 +21,10 @@ root = tk.Tk()
 frame = tk.Frame(root, bg = 'red')
 frame.pack()
 
-button = tk.Button(frame, text = 'Saludar', command = lambda:saludar())
-button.pack()
+button1 = tk.Button(frame, text = 'Saludar', command = lambda:saludar())
+button1.grid(row = 0, column = 0, padx = 5, pady = 5)
+
+button2 = tk.Button(frame, text = 'Escoje un color', command = lambda:colorear())
+button2.grid(row = 0, column = 1, padx = 5, pady = 5)
 
 root.mainloop()
