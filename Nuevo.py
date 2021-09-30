0837211a2be572026708619cfbcaf61bf729caa6
oscarva17 <ojcarvajala@gmail.com>
2021-09-29 21:04:13 -0500

    Updated

--- Nuevo.py
+++ Nuevo.py
@@ -18,7 +18,7 @@ print("Hola mundo")
 print("Hola mundo")
 print("Hola mundo por deiber")
 print("Hola mundo (Daniel H)")
-print("Hola mundo Oscar")
+print('Hola mundo Oscar')
 
 root = tk.Tk()
 root.resizable(0,0)
