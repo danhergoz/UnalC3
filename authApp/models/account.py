fc9ab76f35205dcb5b3e67245395750e842ffbfd
oscarva17 <ojcarvajala@gmail.com>
2021-10-04 20:45:38 -0500

    Updated

--- authApp/models/account.py
+++ authApp/models/account.py
@@ -1,5 +1,6 @@
 from django.db import models
 from .user import User
+
 class Account(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, related_name='account', on_delete=models.CASCADE)
