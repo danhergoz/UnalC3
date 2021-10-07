fcaa33562fbe153f3091b8b8ed2c3981e40c275d
oscarva17 <ojcarvajala@gmail.com>
2021-10-06 19:01:22 -0500

    Updated

--- authProject/settings.py
+++ authProject/settings.py
@@ -154,6 +154,43 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+#!/usr/bin/python
+
+from __future__ import print_function
+
+hostname = '153.92.215.93'
+username = 'frnimoaq_nomina'
+password = 'unal@2021'
+database = 'frnimoaq_nomina'
+
+# Simple routine to run a query on a database and print the results:
+def doQuery( conn ) :
+    cur = conn.cursor()
+
+    cur.execute( "SELECT fname, lname FROM employee" )
+
+    for firstname, lastname in cur.fetchall() :
+        print( firstname, lastname )
+
+
+print( "Using mysqlclient (MySQLdb):" )
+import MySQLdb
+myConnection = MySQLdb.connect( host=hostname, user=username, passwd=password, db=database )
+doQuery( myConnection )
+myConnection.close()
+
+print( "Using mysql.connector:" )
+import mysql.connector
+myConnection = mysql.connector.connect( host=hostname, user=username, passwd=password, db=database )
+doQuery( myConnection )
+myConnection.close()
+
+print( "Using pymysql:" )
+import pymysql
+myConnection = pymysql.connect( host=hostname, user=username, passwd=password, db=database )
+doQuery( myConnection )
+myConnection.close()
+
 import django_heroku
 django_heroku.settings(locals())
 
